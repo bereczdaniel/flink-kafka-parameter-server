066962bf7538912f53e3542db910843d13ad31f4
bdaniel <berecz.17@gmail.com>
2018-08-01 11:13:04 +0200

    update messages

--- src/main/scala/parameter/server/communication/Messages.scala
+++ src/main/scala/parameter/server/communication/Messages.scala
@@ -4,14 +4,24 @@ import parameter.server.utils.Types.{Parameter, ParameterUpdate}
 
 object Messages {
 
-  abstract class Message(src: AnyVal, dest: AnyVal, msg: Option[AnyVal]){
+  abstract class Message(src: AnyVal, dest: AnyVal, msg: Option[Any]){
     def source: AnyVal = src
     def destination: AnyVal = dest
-    def message: Option[AnyVal] = msg
+    def message: Option[Any] = msg
   }
 
-  case class Push(src: AnyVal, dest: AnyVal, msg: ParameterUpdate) extends Message(src, dest, Some(msg))
-  case class Pull(src: AnyVal, dest: AnyVal, msg: Parameter) extends Message(src, dest, Some(msg))
+  case class Push(src: AnyVal, dest: AnyVal, msg: ParameterUpdate) extends Message(src, dest, Some(msg)) {
+    override def toString: String =
+      s"Push:$src:$dest:$msg"
+  }
+  case class Pull(src: AnyVal, dest: AnyVal) extends Message(src, dest, None) {
+    override def toString: String =
+      s"Pull:$src:$dest"
+  }
+  case class PullAnswer(src: AnyVal, dest: AnyVal, msg: Parameter) extends Message(src, dest, Some(msg)) {
+    override def toString: String =
+      s"$src:$dest:$msg"
+  }
 
   class NotSupportedWorkerInput extends Exception
   class NotSupportedMessage extends Exception
