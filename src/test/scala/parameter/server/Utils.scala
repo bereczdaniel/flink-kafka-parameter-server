ef7c8390ebdecab7db35572889f3b5e638c98196
bdaniel <berecz.17@gmail.com>
2018-12-18 14:35:53 +0100

    update tests for async server logic

--- src/test/scala/parameter/server/Utils.scala
+++ src/test/scala/parameter/server/Utils.scala
@@ -3,6 +3,7 @@ package parameter.server
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import parameter.server.communication.Messages.Message
 import parameter.server.utils.Vector
+import parameter.server.utils.Types
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -24,4 +25,20 @@ object CollectSink {
 
   // must be static
   val values: ArrayBuffer[Message[Int, Int, Vector]] = new ArrayBuffer[Message[Int, Int, Vector]]()
+}
+
+// create a testing sink
+class CollectOutputSink[T <: Types.ParameterServerOutput] extends SinkFunction[T] {
+
+  override def invoke(value: T): Unit = {
+    synchronized {
+      CollectOutputSink.values += value
+    }
+  }
+}
+
+object CollectOutputSink {
+
+  // must be static
+  val values: ArrayBuffer[Types.ParameterServerOutput] = new ArrayBuffer[Types.ParameterServerOutput]()
 }
--- src/test/scala/parameter/server/kafka/logic/AsynchronousServerLogicTest.scala
+++ src/test/scala/parameter/server/kafka/logic/AsynchronousServerLogicTest.scala
@@ -9,41 +9,27 @@ import scala.util.Random
 import org.junit.Test
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.PropertyChecks
-import parameter.server.CollectSink
+import parameter.server._
 import parameter.server.communication.Messages
 import parameter.server.communication.Messages.{Message, Pull, PullAnswer, Push}
 import parameter.server.kafka.logic.server.AsynchronousServerLogic
 import parameter.server.utils.{Types, Vector, Utils}
+import parameter.server.kafka.logic.AsynchronousServerLogicTest._
 
 class AsynchronousServerLogicTest extends FlatSpec with PropertyChecks with Matchers {
-
-  
-    val r = new scala.util.Random
-    lazy val numTest = 5
-    lazy val vectorSize = 5
-
-  def generateInput[A](n: Int, f: Int => A): List[A] = {
-    for{
-      seed <- (0 until n).toList
-    } yield f(seed)
-  }
 
   "Asynchronous stateless server logic" should " answer to pull requests" in {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     CollectSink.values.clear()
     
-    val data = generateInput(numTest, seed => Pull[Int, Int, Vector](seed, r.nextInt(numTest)))
+    val data = generateInput(numTest, seed => Pull[Int, Int, Vector](r.nextInt(numTest), r.nextInt(numTest)))
 
     val ds: DataStream[Message[Int, Int, Vector]] = env
       .fromCollection(data)
 
-    lazy val udf: (Int, Int) => Vector = (x,y) => Vector(Array(x.toDouble, y.toDouble, math.pow(x,y))) 
-
-    ds
+    val output = ds
       .process(new AsynchronousServerLogic[Int, Int, Vector] {
         override val model: ValueState[Vector] = null
-
-        lazy val udf: (Int, Int) => Vector = (x,y) => Vector(Array(x.toDouble, y.toDouble, math.pow(x,y))) 
 
         override def onPullReceive(pull: Pull[Int, Int, Vector], out: Collector[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]]]): Unit = {
           val payload: Vector = udf(pull.dest, pull.src)
@@ -52,37 +38,26 @@ class AsynchronousServerLogicTest extends FlatSpec with PropertyChecks with Matc
 
         override def onPushReceive(push: Messages.Push[Int, Int, Vector], out: Collector[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]]]): Unit = ???
       })
-      .flatMap(new FlatMapFunction[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]], Message[Int, Int, Vector]] {
-        override def flatMap(value: Either[Types.ParameterServerOutput, Message[Int, Int, Vector]], out: Collector[Message[Int, Int, Vector]]): Unit = {
-          value match {
-            case Left(_) =>
-            case Right(msg) => out.collect(msg)
-          }
-        }
-      })
+      
+      val (_, msgStream) = Utils.splitStream(output)
+
+      msgStream
       .addSink(new CollectSink[Message[Int, Int, Vector]])
 
 
     env.execute()
 
-
-    assert(
-      CollectSink.values.toList.sortBy(_.destination) == 
-      data.map(x => PullAnswer(x.dest, x.src, udf(x.dest, x.src))).sortBy(_.destination))
+    CollectSink.values.toList.sortBy(m => (m.destination, m.source)) shouldBe data.map(x => PullAnswer(x.dest, x.src, udf(x.dest, x.src))).sortBy(m => (m.destination, m.source))
   }
 
-  "Asnychronous server logic" should "update its state from push messages" in {
+  "Asnychronous server logic" should "update its state based on push messages" in {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     CollectSink.values.clear()
-
-
-    lazy val vectorSizeFlink = vectorSize
     
     val data = generateInput(numTest, seed => Push[Int, Int, Vector](r.nextInt(numTest), r.nextInt(numTest), Vector(Array.fill(vectorSize)(scala.util.Random.nextDouble))))
 
     val ds: DataStream[Message[Int, Int, Vector]] = env
       .fromCollection(data)
-
 
     val output = ds
       .keyBy(p => p.destination)
@@ -93,7 +68,7 @@ class AsynchronousServerLogicTest extends FlatSpec with PropertyChecks with Matc
         override def onPullReceive(pull: Pull[Int, Int, Vector], out: Collector[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]]]): Unit = ???
 
         override def onPushReceive(push: Messages.Push[Int, Int, Vector], out: Collector[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]]]): Unit = {
-          val p = getOrElseUpdate(Vector(Array.fill(5)(0.0)))
+          val p = getOrElseUpdate(Vector(Array.fill(vectorSize)(0.0)))
 
           val update = Vector.vectorSum(p, push.msg)
 
@@ -120,9 +95,67 @@ class AsynchronousServerLogicTest extends FlatSpec with PropertyChecks with Matc
       .groupBy(m => m.source)
       .map({
         case(k,v) => 
-        k -> v.foldLeft(Vector(Array.fill(vectorSize)(0.0))){(acc, i) => Vector.vectorSum(acc, i.message.get)}})
+        k -> (v flatMap(_.message) maxBy(Vector.vectorLengthSqr))
+      })
 
     result shouldBe target
+  }
+
+  "Asynchronous server logic" should "write to output" in {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    CollectSink.values.clear()
+    
+    val data = generateInput(numTest, seed => Pull[Int, Int, Vector](r.nextInt(numTest), r.nextInt(numTest)))
+
+    val ds: DataStream[Message[Int, Int, Vector]] = env
+      .fromCollection(data)
+
+    val output = ds
+      .process(new AsynchronousServerLogic[Int, Int, Vector] {
+        override val model: ValueState[Vector] = null
+
+        override def onPullReceive(pull: Pull[Int, Int, Vector], out: Collector[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]]]): Unit = {
+          val payload: Vector = udf(pull.dest, pull.src)
+          out.collect(Left(DummyOutput(payload)))
+        }
+
+        override def onPushReceive(push: Messages.Push[Int, Int, Vector], out: Collector[Either[Types.ParameterServerOutput, Message[Int, Int, Vector]]]): Unit = ???
+      })
+      
+      val (outStream, _) = Utils.splitStream(output)
+
+      outStream
+      .addSink(new CollectOutputSink[Types.ParameterServerOutput])
+
+
+    env.execute()
+
+    CollectOutputSink.values.toList.map(_.asInstanceOf[DummyOutput].v).sortBy(Vector.vectorLengthSqr) shouldBe data.map(x => udf(x.dest, x.src)).sortBy(Vector.vectorLengthSqr)
+    
+  }
+}
+
+object AsynchronousServerLogicTest {
+  
+    val r = new scala.util.Random
+    lazy val numTest = 20
+    lazy val vectorSize = 5
+
+    val udf: (Int, Int) => Vector = (x,y) => Vector(Array(x.toDouble, y.toDouble, math.pow(x,y))) 
+
+    case class DummyOutput(v: Vector) extends Types.ParameterServerOutput
+
+  /**
+    * Generate n input with the provided function
+    * @param n: Number of generated instances
+    * @param f: Function to instanstiate input
+    * @return List of inputs
+    */
+  def generateInput[A](n: Int, f: Int => A): List[A] = {
+    for{
+      seed <- (0 until n).toList
+    } yield f(seed)
   }
 
 }
