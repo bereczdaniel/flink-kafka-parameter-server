87b75df705379e60805774ef2b1ec57a996a9f6d
Molnár András J <modras@ilab.sztaki.hu>
2019-01-02 15:47:51 +0100

    Redis push-update lua script works for uninitialized user vectors.

--- src/main/resources/scripts/redis/push_update_user_vector.lua
+++ src/main/resources/scripts/redis/push_update_user_vector.lua
@@ -1,6 +1,7 @@
 local userKey = KEYS[1]
 local userDelta = ARGV
 
+--No existence checking needed: if the target value is not set yet, it is initialized to 0 by redis.
 for i=1,#userDelta,1 do
     redis.call("HINCRBYFLOAT", userKey, i-1, userDelta[i])
 end
