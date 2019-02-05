24347c19faf203eb90ea5b77903bfb2a0db2d5ec
bdaniel <berecz.17@gmail.com>
2019-02-05 15:18:29 +0100

    Model base class

---
+++ src/main/scala/parameter/server/logic/worker/Model.scala
@@ -0,0 +1,54 @@
+package parameter.server.logic.worker
+
+/**
+  * Represents a machine learning model.
+  * @tparam K: Type of the key
+  * @tparam P: Type of the parameter
+  */
+abstract class Model[K,P]() {
+
+  /**
+    * Binary operator, on how to combine two parameter
+    * @return The combined value of the two parameter
+    */
+  def updateFunction: (P,P) => P
+
+  def initFunction: K => P
+
+  /**
+    * Returns the corresponding the value, if it doesn't exist, then init
+    * @param key
+    * @return
+    */
+  def get(key: K): Option[P]
+
+  /**
+    * Set the value of key to newValue. Override if there is already one.
+    * @param key
+    * @param newValue
+    */
+  def set(key: K, newValue: P): Unit
+
+  /**
+    * Update the existing value with the newValue using the updateFunction. If there is no original value, one will be
+    * initialized and used as original.
+    * @param key
+    * @param newValue
+    */
+  def updateWith(key: K, newValue: P): Unit =
+    set(key, updateFunction(getOrElseInit(key) , newValue))
+
+  /**
+    * Get the value of the key, If there is no corresponding value, initialize it.
+    * @param key
+    * @return
+    */
+  def getOrElseInit(key: K): P =
+    get(key) match {
+      case Some(orig) => orig
+      case None =>
+        val newValue = initFunction(key)
+        set(key, newValue)
+        newValue
+    }
+}
