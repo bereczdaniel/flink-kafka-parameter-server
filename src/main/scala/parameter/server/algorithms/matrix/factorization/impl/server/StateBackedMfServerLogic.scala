a8b966b983e24042561e33f8107ca26aa2d29a25
bdaniel <berecz.17@gmail.com>
2019-02-20 17:51:21 +0100

    update server logic to handle model load

--- src/main/scala/parameter/server/algorithms/matrix/factorization/impl/server/StateBackedMfServerLogic.scala
+++ src/main/scala/parameter/server/algorithms/matrix/factorization/impl/server/StateBackedMfServerLogic.scala
@@ -2,10 +2,10 @@ package parameter.server.algorithms.matrix.factorization.impl.server
 
 import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.util.Collector
+import parameter.server.algorithms.matrix.factorization.Types.ItemId
 import parameter.server.communication.Messages
 import parameter.server.communication.Messages.PullAnswer
 import parameter.server.logic.server.AsynchronousServerLogic
-import parameter.server.utils.Types.ItemId
 import parameter.server.utils.{Types, Vector}
 
 class StateBackedMfServerLogic(_init: Int => Vector, _update: (Vector, Vector) => Vector)
@@ -25,7 +25,11 @@ class StateBackedMfServerLogic(_init: Int => Vector, _update: (Vector, Vector) =
                              out: Collector[Either[Types.ParameterServerOutput, Messages.Message[Int, Long, Vector]]]): Unit = {
     val oldParam = model.value()
 
-    model.update(update(oldParam, push.msg))
+    //TODO log or somehow signal the chosen path maybe?
+    if(oldParam != null)
+      model.update(update(oldParam, push.msg))
+    else
+      model.update(push.msg)
   }
 }
 
