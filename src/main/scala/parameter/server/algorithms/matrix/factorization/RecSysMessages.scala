a4be438d88770f18520c1b1cf363356f619c123c
bdaniel <berecz.17@gmail.com>
2019-03-05 15:29:36 +0100

    model parse functions

--- src/main/scala/parameter/server/algorithms/matrix/factorization/RecSysMessages.scala
+++ src/main/scala/parameter/server/algorithms/matrix/factorization/RecSysMessages.scala
@@ -6,26 +6,28 @@ import parameter.server.utils.Vector
 
 object RecSysMessages {
 
-  case class Rating(userId: UserId, itemId: ItemId, rating: Double) extends WorkerInput(userId)
+  case class Rating(userId: UserId, itemId: ItemId, rating: Double) extends WorkerInput(itemId)
 
-  //TODO temporary solution, do a refactor!!!!!
-  case class Temp(data: Either[EvaluationRequest, ModelParameter]) extends WorkerInput {
-    override val destination: AnyVal =
-      data match {
-        case Left(eval) => eval.destination
-        case Right(mp) => mp.destination
-      }
-  }
-  case class EvaluationRequest(userId: Int, itemId: Int, evaluationId: Long, rating: Double, ts: Long) extends WorkerInput(userId)
-  sealed abstract class ModelParameter(id: Int, param: Vector) extends WorkerInput(id)
-  case class UserParameter(userId: Int, parameter: Vector) extends ModelParameter(userId, parameter)
-  case class ItemParameter(itemId: Int, parameter: Vector) extends ModelParameter(itemId, parameter)
+  case class EvaluationRequest(userId: Int, itemId: Int, evaluationId: Long, rating: Double, ts: Long) extends WorkerInput(itemId)
+  case class UserParameter(userId: Int, parameter: Vector) extends WorkerInput(userId)
+  case class ItemParameter(itemId: Int, parameter: Vector) extends WorkerInput(itemId)
   case class NegativeSample(userId: UserId, itemId: ItemId, rating: Double) extends WorkerInput
 
   case class VectorModelOutput(id: AnyVal, parameter: Vector) extends ParameterServerOutput {
     override def toString: String =
       id.toString + ":" + parameter.value.tail.foldLeft(parameter.value.head.toString)((acc, c) => acc + "," + c.toString)
   }
+
+  def parseUserParameter(line: String): WorkerInput = {
+    val id :: params = line.split(",").toList
+    UserParameter(id.toInt, Vector(params.map(_.toDouble).toArray))
+  }
+
+  def parseItemParameter(line: String): WorkerInput = {
+    val id :: params = line.split(",").toList
+    ItemParameter(id.toInt, Vector(params.map(_.toDouble).toArray))
+  }
+
 
   case class EvaluationOutput(itemId: Int, evaluationId: Long, topK: List[Prediction], ts: Long) extends ParameterServerOutput
 }
